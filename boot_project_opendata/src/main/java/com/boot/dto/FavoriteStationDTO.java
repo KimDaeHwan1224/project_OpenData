c9a100a6ae9e2b57a978b70b219ad1bfbd46bdda
jseulmi <127377332+jseulmi@users.noreply.github.com>
2025-11-27 16:15:52 +0900

    Update FavoriteStationDTO.java

--- boot_project_opendata/src/main/java/com/boot/dto/FavoriteStationDTO.java
+++ boot_project_opendata/src/main/java/com/boot/dto/FavoriteStationDTO.java
@@ -9,18 +9,11 @@ public class FavoriteStationDTO {
     private Long favoriteId;
     private String userId;
 
-    private String stationName;  // 지역
-    private Double dmY;          // 위도
-    private Double dmX;          // 경도
+//    private Long stationId;      // ✅ FK
+    private String stationName;  // 화면 표시용
 
-    // 대기질 데이터
-    private Integer pm10Value;   // PM10
-    private Integer pm25Value;   // PM2.5
-    private Double o3Value;      // 오존
-    private Double no2Value;     // 이산화질소
-    private Double coValue;      // 일산화탄소
-    private Double so2Value;     // 아황산가스
+    private Integer pm10Value;
 
     private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+    private LocalDateTime updatedAt; // 있으면 유지
 }
